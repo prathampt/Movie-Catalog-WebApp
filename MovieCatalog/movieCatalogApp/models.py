9fdaadc44ae6a6c38d9d85175ba656804d826049
Pratham Tandale <prathamt12345@gmail.com>
2023-08-12 16:33:31 +0530

    requests and api implementation

--- MovieCatalog/movieCatalogApp/models.py
+++ MovieCatalog/movieCatalogApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    release_date = models.DateField()
+    description = models.TextField()
+    director = models.CharField(max_length=100)
+    duration_minutes = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.title} {self.rating}/5.0"
+
+class UserBookmark(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)#-----------------
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(choices=[(1, '1 Star'), (2, '2 Stars'), (3, '3 Stars'), (4, '4 Stars'), (5, '5 Stars')])
+    comments = models.TextField(blank=True)
+    bookmarked_at = models.DateTimeField(auto_now_add=True)
+    watched = models.BooleanField(default=False)
+    watched_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.movie} is bookmarked by {self.user}"
--- MovieCatalog/movieCatalogApp/views.py
+++ MovieCatalog/movieCatalogApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from . import utils
 
 def index(request):
-    return render(request, 'movieCatalogApp/index.html')
+    return render(request, 'movieCatalogApp/index.html', {
+        "movie" : utils.fetch_movie_from_omdb() # take movies from db
+    })
 
 def profile(request):
     return render(request, 'movieCatalogApp/profile.html')
